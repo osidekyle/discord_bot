fc0e08ef861f81022c45320295de6fddbcb488a4
osidekyle <hawkinskyle67@gmail.com>
2021-01-20 18:36:12 -0800

    ping pong

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello, World")
+require("dotenv").config()
+
+
+const Discord=require("discord.js")
+
+const client= new Discord.Client()
+
+client.on("ready",function(){
+    console.log("Hello")
+})
+
+client.on("message",function(message){
+    if(message.content=="ping"){
+        
+    message.channel.send("pong")
+    }
+})
+
+
+
+
+client.login(process.env.DISCORD_TOKEN)
+
